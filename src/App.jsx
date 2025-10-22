3da378fdf8b6d1075441e4b3d108c20216875155
Alvin Muira <alvinmuira@gmail.com>
2025-10-23 00:01:05 +0300

    Updated title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Home from './Home.jsx';
+import NavBar from './NavBar.jsx';
+import Footer from './Footer.jsx';
 
 function App() {
-  return
-  <>
-  <Home />
-  </>
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
